f099cda876c928cfa2c0404c027fc754cb77b8a0
Matt <38562764+LavMatt@users.noreply.github.com>
2022-02-22 15:43:10 +0000

    Create moj_package_names.py

---
+++ moj_package_names.py
@@ -0,0 +1,31 @@
+import argparse
+
+
+parser = argparse.ArgumentParser(
+    description="gets packages minus package of repo test is run on"
+)
+
+parser.add_argument(
+    "--repo",
+    help="the name of the current github repository"
+)
+
+args = parser.parse_args()
+
+repo_name = args.repo
+
+# (pypi-name, repo-name)
+mojap_packages = [
+    ("mojap-metadata", "mojap-metadata"),
+    ("pydbtools", "pydbtools"),
+    ("data-linter", "data_linter"),
+    ("s3-data-packer", "s3_data_packer"),
+    ("arrow-pd-parser", "mojap-arrow-pd-parser"),
+    ("gluejobutils", "gluejobutils"),
+    ("dataengineeringutils3", "dataengineeringutils3"),
+    ("ml-sandbox", "ml-sandbox")
+]
+
+packages_to_install = [p[0] for p in mojap_packages if p[1] != repo_name]
+
+print("pip install " + " ".join(packages_to_install))
